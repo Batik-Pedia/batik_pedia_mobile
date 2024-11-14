69002bb8b5198a957df25e4c4cd5cef2e6787eb1
Lamz16 <andiku0755@gmail.com>
2024-11-15 02:57:25 +0700

    remove unused import

--- app/src/main/java/com/tricakrawala/batikpedia/presentation/ui/screen/wisata/WisataScreen.kt
+++ app/src/main/java/com/tricakrawala/batikpedia/presentation/ui/screen/wisata/WisataScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
